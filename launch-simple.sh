7fc8b8f965852b47dbdde093449ef388b92a9de9
Teemu Itkonen <teemu.h.itkonen@gmail.com>
2016-03-11 16:24:28 +0200

    redirect datastream

--- launch-simple.sh
+++ launch-simple.sh
@@ -3,9 +3,15 @@
 trap 'kill -HUP 0' EXIT
 exec 3>&1
 
-./websocketd --port=8002 --address=localhost bash -c "tee 1>&3" 1>&2 &
+datadir=$PWD"/../easyrider/data/"
+outpath=$datadir$(date +%Y%m%d-%H-%M-%S)-$1.json
 
-primusrun chromium --user-data-dir=chromium-data --allow-file-access-from-files "file://$PWD/index.html?disableDefaultLogger=true&wsLogger=ws://localhost:8002"
+#./websocketd --port=8002 --address=localhost bash -c "tee 1>&3" 1>&2 &
+./websocketd --port=8002 --address=localhost bash -c "tee 1>$outpath" 1>&2 &
+
+BROWSER="primusrun chromium --user-data-dir=chromium-data --allow-file-access-from-files" 
+
+$BROWSER "file://$PWD/index.html?disableDefaultLogger=true&wsLogger=ws://localhost:8002"
 
 #primusrun chromium --user-data-dir=chromium-data --allow-file-access-from-files index.html
 
